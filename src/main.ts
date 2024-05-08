418b55e25ed654577b6435cac9053c1b880081c4
Mona Lisa <johndoe@example.com>
2024-05-08 02:05:19 -0600

    dsadasd

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   //const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Ignorar datos que no esten en los DTO
