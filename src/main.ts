50ea15a7c51a19e27845a6922e021fce9be30cb5
Mona Lisa <johndoe@example.com>
2024-05-08 01:57:48 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  //const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  //app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Ignorar datos que no esten en los DTO
