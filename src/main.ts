389b1c0e4dd0b92bceb0df5594a96968488e1a64
Mona Lisa <johndoe@example.com>
2024-05-09 22:20:44 -0600

    asd

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Ignorar datos que no esten en los DTO
