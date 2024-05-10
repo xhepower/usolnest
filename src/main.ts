10f860199a880cdcddd8fbc8d289f73057e9d76d
Mona Lisa <johndoe@example.com>
2024-05-09 21:56:22 -0600

    cxaszcxzvc

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   //const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create(AppModule, {
     cors: true,
-    forceCloseConnections: true,
   });
   app.enableCors({
     origin: '*',
